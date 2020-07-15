19be54bc5fedef3eca8d8bec3f02a311ba8c621a
Leslie G <lesliegonzalez@me.com>
2020-07-15 20:24:45 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -7,7 +7,7 @@ def third_challenge
       :hero_friends => []
    }, 
    :capulet => {
-      :patriarch => {
+      :patriarch => {:name "Lord Capulet", 
         #your key/value pairs here
         },
       :matriarch => {
